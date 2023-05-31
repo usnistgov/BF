3c385e6281a64eee6c882c730680d79b456e2acd
Farhan Nadeem <farhannadiim@gmail.com>
2023-05-31 01:08:34 -0400

    Carousel fix

--- assets/js/bootstrap.js
+++ assets/js/bootstrap.js
@@ -1,2 +1,9 @@
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
 // import 'bootstrap/dist/js/bootstrap.min.js'
+
+var carousel1 = $('#carouselControls').carousel();
+var carousel2= $('#carouselControls2').carousel();
+carousel1.on('slide.bs.carousel', function(event) {
+    var to = $(event.relatedTarget).index();
+    carousel2.carousel(to);
+});
